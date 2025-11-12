8ef40935db0e19c89f69405b62bf182b7ffd9393
Nate Gonzales-Hess <nhess@uoregon.edu>
2025-11-12 00:32:53 -0800

    sleap-nn

--- sleap-nn_batch.py
+++ sleap-nn_batch.py
@@ -3,10 +3,10 @@ import subprocess
 import os
 from pathlib import Path
 
-experiment = 'clickbait-motivate'
+experiment = 'clickbait-visual'
 
-model_path = f"C:/clickbait-sleap/clickbait-motivate-sparse-v2/models/n=1057"
-bonsai_video_paths = get_file_paths(f'C:/{experiment}/bonsai/nov9/', 'avi', '', print_paths=True, print_n=5)
+model_path = f"S:/sfn-poster/sleap-nn/clickbait-motivate-sparse-v2/models/n=1057"
+bonsai_video_paths = get_file_paths(f'S:/sfn-poster/sleap-nn/7004', 'avi', '', print_paths=True, print_n=5)
 
 def run_sleap():
     sleap_command = 'sleap-nn track'
@@ -16,7 +16,7 @@ def run_sleap():
     # Run sleap inference on each video
     for video_path in bonsai_video_paths:
         output_dir = video_path.parent
-        cmd = f"conda run -n sleap {sleap_command} --data_path {str(video_path)} --model_paths {model_path}"
+        cmd = f"conda run -n sleap-25 {sleap_command} --data_path {str(video_path)} --model_paths {model_path}"
         print(f"Running: {cmd}")
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         print(result.stdout)
